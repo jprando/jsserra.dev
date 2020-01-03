e9b1c5bf3797340fc2a3c1a0923244bc9d101758
Jeudi Prando <jeudiprando@gmail.com>
2020-01-03 08:51:22 -0300

    ajustes de margens

--- src/components/JsSerraBanner.vue
+++ src/components/JsSerraBanner.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="hello">
     <img src="./../assets/jsserra.camisa.jpeg" alt="JSSerra Camisa" srcset="">
-    <h1>O que fazemos</h1>
-    <p> Tecnologia Web na Serra-ES </p>
+    <h1>O QUE FAREMOS</h1>
+    <h2> Tecnologia Web na Serra-ES </h2>
     <p>Comunidade criada e mantida com a finalidade de unir pessoas que gostam ou usam tecnologia para a web e que moram na Serra-ES, se você é Dev ou entusiasta, ou até mesmo gosta do assunto é muito bem vindo!</p>
     <p>Vamos aprender, fazer acontecer, evoluir e falar sobre...</p>
     <p>JUNTOS :D</p>
-    <p>Telegram <a href="https://t.me/jsserra" target="_blank" rel="noopener">https://t.me/jsserra</a></p>
-    <p>Meetup <a href="https://www.meetup.com/pt-BR/jsserra/" target="_blank" rel="noopener">https://www.meetup.com/pt-BR/jsserra/</a></p>
+    <p>Telegram <br> <a href="https://t.me/jsserra" target="_blank" rel="noopener">https://t.me/jsserra</a></p>
+    <p>Meetup <br> <a href="https://www.meetup.com/pt-BR/jsserra/" target="_blank" rel="noopener">https://www.meetup.com/pt-BR/jsserra/</a></p>
     <p>Seja bem vindo ao clube <strong>JSSERRA</strong></p>
   </div>
 </template>
@@ -22,7 +22,7 @@ export default {
 <style scoped>
 img {
   object-fit: cover;
-  object-position: 00% -150px;
+  object-position: 0px -150px;
   width: 800px;
   height: 125px;
 }
@@ -45,5 +45,17 @@ a {
   margin: auto;
   background-color: white;
   padding-bottom: 100px;
+}
+.hello p{
+  padding-left: 20px;
+  padding-right: 20px;
+}
+.hello h1{
+  padding-left: 20px;
+  padding-right: 20px;
+}
+.hello h2{
+  padding-left: 20px;
+  padding-right: 20px;
 }
 </style>
